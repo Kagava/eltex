fd3efa3b6b5013d6c0dce2f37d357b3ec8dcd534
Kagava <esspakov@mail.ru>
2026-03-22 21:13:19 +0700

    fix: change class names and add semantic

--- blog/src/scripts/blog.js
+++ blog/src/scripts/blog.js
@@ -2,8 +2,8 @@ import { articlesCount } from "./parts/statistic.js";
 
 const body = document.body;
 const stub = document.querySelector(".black-back");
-const statisticButton = document.querySelector(".dialog__stat");
-const modalStatistic = document.querySelector(".info");
+const statisticButton = document.querySelector(".admin-panel__stat");
+const modalStatistic = document.querySelector(".dialog");
 const spanArticleCount = document.querySelector(".articles-modal__counter");
 
 statisticButton.addEventListener("click", () => {
