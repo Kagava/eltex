1313e141f9f7f29645e38836494fca47db49c72d
Kagava <esspakov@mail.ru>
2026-03-26 15:21:21 +0700

    feat: add handling close button click

--- blog/src/scripts/parts/deleteArticle.js
+++ blog/src/scripts/parts/deleteArticle.js
@@ -14,5 +14,30 @@ function addCloseButton(article) {
 function createCloseButton() {
   const closeButton = document.createElement("div");
   closeButton.className = "close-button article__close";
+  closeButton.addEventListener("click", closeArticle.bind(closeButton));
   return closeButton;
+}
+
+function closeArticle(event) {
+  const target = event.target;
+  const targetArticle = target.closest(".article");
+  if (targetArticle.classList.contains("article-main")) {
+    console.log(targetArticle);
+    targetArticle.remove();
+    deleteMain();
+  }
+}
+
+function deleteMain() {
+  const firstArtilce = document.querySelector(".article");
+  firstArtilce.classList.remove("article_small");
+  firstArtilce.classList.add("article-main");
+  const fotoArticle = firstArtilce.querySelector(".article__foto");
+  fotoArticle.classList.add("foto-main");
+  const textArticle = Array.from(
+    firstArtilce.querySelector(".article__text").children,
+  );
+  textArticle.forEach((item) => {
+    item.classList.value = item.classList.value.replace(/\w+_closed/, "");
+  });
 }
