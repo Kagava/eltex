4080897eb6800e8f09a5eba62af18d6c5310a952
Kagava <esspakov@mail.ru>
2026-03-31 18:27:15 +0700

    feat: add loader layout and animation

---
+++ blog/src/scripts/parts/loader.js
@@ -0,0 +1,11 @@
+const loader = document.querySelector(".loader");
+
+// let angle = 0;
+// setTimeout(function rotate() {
+//   loader.style.transform = `rotate(${angle}deg)`;
+//   if (angle === 361) {
+//     angle = 0;
+//   }
+//   angle += 1;
+//   setTimeout(rotate, 10);
+// }, 10);
