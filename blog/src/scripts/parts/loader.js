1b3b27e1d3b6b9c6ac031666745c1f761f6db5ad
Kagava <esspakov@mail.ru>
2026-03-31 20:25:29 +0700

    fix: fix loader behavior after it've been show down

--- blog/src/scripts/parts/loader.js
+++ blog/src/scripts/parts/loader.js
@@ -1,12 +1,15 @@
 const loader = document.querySelector(".loader");
 const curtain = document.querySelector(".black-back");
+const adrminPanel = document.querySelector(".admin-panel");
 
 export function openLoader() {
   loader.classList.remove("is-hidden");
   curtain.classList.remove("is-hidden");
+  adrminPanel.style.zIndex = "1";
 }
 
 export function closeLoader() {
+  adrminPanel.style.zIndex = "2";
   loader.classList.add("is-hidden");
   curtain.classList.add("is-hidden");
 }
