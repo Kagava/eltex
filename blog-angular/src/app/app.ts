247fe49f026cf1c4a99615efea259cfdeb12f268
Kagava <esspakov@mail.ru>
2026-04-02 18:20:00 +0700

    feat: add add main

--- blog-angular/src/app/app.ts
+++ blog-angular/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './ui/components/header/header';
 import { Footer } from './ui/components/footer/footer';
+import { MainPage } from './ui/pages/main-page/main-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, MainPage],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
