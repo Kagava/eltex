12c71fcee4072743a48250a81f22a9ee2c9e1ef7
Kagava <esspakov@mail.ru>
2026-04-03 11:04:21 +0700

    feat: add my-career block

--- blog-angular/src/app/ui/pages/main-page/main-page.ts
+++ blog-angular/src/app/ui/pages/main-page/main-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { About } from './parts/about/about';
+import { Information } from './parts/information/information';
 
 @Component({
   selector: 'app-main-page',
-  imports: [About],
+  imports: [About, Information],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
--- blog-angular/src/app/ui/pages/main-page/parts/information/information.ts
+++ blog-angular/src/app/ui/pages/main-page/parts/information/information.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Career } from './career/career';
 
 @Component({
   selector: 'app-information',
-  imports: [],
+  imports: [Career],
   templateUrl: './information.html',
   styleUrl: './information.scss',
 })
