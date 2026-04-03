606a99b8d68d90447116a9afa07071d794254ddf
Kagava <esspakov@mail.ru>
2026-04-03 11:41:34 +0700

    feat: add works block

--- blog-angular/src/app/ui/pages/main-page/parts/information/information.ts
+++ blog-angular/src/app/ui/pages/main-page/parts/information/information.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Career } from './career/career';
 import { Hobby } from './hobby/hobby';
 import { Articles } from './articles/articles';
+import { Works } from './works/works';
 
 @Component({
   selector: 'app-information',
-  imports: [Career, Hobby, Articles],
+  imports: [Career, Hobby, Articles, Works],
   templateUrl: './information.html',
   styleUrl: './information.scss',
 })
