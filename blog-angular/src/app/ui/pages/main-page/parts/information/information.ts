c81826e34ecedc3ec64fb16ad44d3ff034bd168b
Kagava <esspakov@mail.ru>
2026-04-03 11:26:45 +0700

    feat: add articles block

--- blog-angular/src/app/ui/pages/main-page/parts/information/information.ts
+++ blog-angular/src/app/ui/pages/main-page/parts/information/information.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Career } from './career/career';
 import { Hobby } from './hobby/hobby';
+import { Articles } from './articles/articles';
 
 @Component({
   selector: 'app-information',
-  imports: [Career, Hobby],
+  imports: [Career, Hobby, Articles],
   templateUrl: './information.html',
   styleUrl: './information.scss',
 })
