5f91071b849800774d785ffa1f7d6f057060d061
Kagava <esspakov@mail.ru>
2026-04-16 21:46:28 +0700

    feat: make custom select work

--- blog-angular/src/app/ui/components/add-article-form/add-article-form.ts
+++ blog-angular/src/app/ui/components/add-article-form/add-article-form.ts
@@ -8,4 +8,44 @@ import { Component, input } from '@angular/core';
 })
 export class AddArticleForm {
   public toggleForm = input<boolean>();
+  protected isSelectOpen: boolean;
+  private transform: number = 42;
+  protected spanSelectValue: string = 'Tennis';
+  constructor() {
+    this.isSelectOpen = false;
+  }
+
+  protected onSubmit(e: Event) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  protected openCustomeSelect(event: Event) {
+    const target = event.target;
+    this.isSelectOpen = !this.isSelectOpen;
+  }
+
+  protected tennisChoice(event: Event) {
+    event.stopPropagation();
+    this.isSelectOpen = !this.isSelectOpen;
+    this.spanSelectValue = 'Tennis';
+  }
+
+  protected frontendChoice(event: Event) {
+    event.stopPropagation();
+    this.isSelectOpen = !this.isSelectOpen;
+    this.spanSelectValue = 'Frontend';
+  }
+
+  protected transformString(value: number) {
+    if (this.isSelectOpen) {
+      return `translateY(${value * 100 + this.transform}%)`;
+    } else {
+      return `translateY(-75%)`;
+    }
+  }
+
+  protected checkDefault(value: string) {
+    return value === this.spanSelectValue;
+  }
 }
