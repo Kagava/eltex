0c1edec834acc47d0dd77d603b968a3073b4026f
Kagava <esspakov@mail.ru>
2026-04-20 12:07:08 +0700

    feat: add style and appear condition for articles

--- blog-angular/src/app/ui/components/article-component/article-component.ts
+++ blog-angular/src/app/ui/components/article-component/article-component.ts
@@ -1,14 +1,16 @@
 import { Component, input, output } from '@angular/core';
 import { Article } from '../../../models/types/articles';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-article-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './article-component.html',
   styleUrl: './article-component.scss',
 })
 export class ArticleComponent {
   public articleArray = input<Article[]>();
   public articleToDelete = output<string>();
+  public articlePage = input<boolean>();
   removeArticle(id: string) {
     this.articleToDelete.emit(id);
   }
