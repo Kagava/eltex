dd01eac42ee43d7ce0d3bc99ac64f6684d68376f
Kagava <esspakov@mail.ru>
2026-05-05 13:44:51 +0700

    feat: add finding id in the article page

--- blog-angular/src/app/ui/pages/article-page/article-page.ts
+++ blog-angular/src/app/ui/pages/article-page/article-page.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-article-page',
   imports: [],
   templateUrl: './article-page.html',
   styleUrl: './article-page.scss',
 })
-export class ArticlePage {}
+export class ArticlePage {
+  private activeRouter = inject(ActivatedRoute);
+  private destroyRef$ = inject(DestroyRef);
+  protected articleId: string = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.activeRouter.params
+      .pipe(takeUntilDestroyed(this.destroyRef$))
+      .subscribe((obj) => (this.articleId = obj['id']));
+  }
+}
